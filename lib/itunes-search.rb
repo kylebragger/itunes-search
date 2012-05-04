0d11dd5d8829c89a1029bff444e19bb0ede15cd8
Kyle Bragger <kyle.bragger@gmail.com>
2012-05-04 12:56:28 -0400

    add hacky lookup support

--- lib/itunes-search.rb
+++ lib/itunes-search.rb
@@ -19,4 +19,5 @@ end
 directory = File.expand_path(File.dirname(__FILE__))
 require File.join(directory,"itunes-search", "base")
 require File.join(directory,"itunes-search","search")
+require File.join(directory,"itunes-search","lookup")
 require File.join(directory,"itunes-search","result")
--- lib/itunes-search/base.rb
+++ lib/itunes-search/base.rb
@@ -1,11 +1,16 @@
 module ItunesSearch 
 
   ENDPOINT = "http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStoreServices.woa/wa/wsSearch"
+  LOOKUP_ENDPOINT = "http://itunes.apple.com/lookup"
   
   class Base
    
     def search(*args)
       return ItunesSearch::Search.new(*args)  
     end  
+    
+    def lookup(*args)
+      return ItunesSearch::Lookup.new(*args)
+    end
   end
 end
